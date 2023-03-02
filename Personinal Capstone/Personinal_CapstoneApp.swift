0a2c85357569ef59a6ffdf7edd0366f8c0574904
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2023-03-02 07:40:40 -0700

    Initial Commit

---
+++ Personinal Capstone/Personinal_CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personinal_CapstoneApp.swift
+//  Personinal Capstone
+//
+//  Created by Vasiliy on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Personinal_CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
