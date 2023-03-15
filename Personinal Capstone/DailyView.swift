f125b117be166c2bf71837242494c28e2d24b6e7
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2023-03-15 10:20:29 -0600

    second verson

---
+++ Personinal Capstone/DailyView.swift
@@ -0,0 +1,8 @@
+//
+//  DailyView.swift
+//  Personinal Capstone
+//
+//  Created by Vasiliy on 3/15/23.
+//
+
+import Foundation
---
+++ Personinal Capstone/SecondAddEditView.swift
@@ -0,0 +1,8 @@
+//
+//  SecondAddEditView.swift
+//  Personinal Capstone
+//
+//  Created by Vasiliy on 3/15/23.
+//
+
+import Foundation
